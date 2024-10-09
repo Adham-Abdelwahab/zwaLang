6cc07e6899d572f468c51252e22e74841f0e7f41
Matthew Mierzwa <mtm45@njit.edu>
2024-10-09 01:12:53 -0400

    first commit of zwa lang - beginning work on Lexer

---
+++ src/Lexer/Lexer.go
@@ -0,0 +1,29 @@
+package Lexer
+
+import (
+	"fmt"
+	"unicode"
+)
+
+type Lexer struct {
+	input 			string
+	position 		int // current position in input (points to current char)
+	readPosition 	int // reading position (after current char)
+	ch 				byte // current char under examination
+}
+
+// NewLexer initializes a new lexer with the given input string
+func NewLexer(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l;
+}
+
+func main() {
+	testInputString := `x: number = 10
+						y: number = 20
+						z = x + y
+						show z`
+	
+	fmt.Println(testInputString);
+}
