8c46778c24fe03b87e251b74592e1619dbf65281
Matthew Mierzwa <mtm45@njit.edu>
2024-10-11 00:36:09 -0400

    Parser built

---
+++ src/parser/parser.go
@@ -0,0 +1,137 @@
+package parser
+
+import(
+	"fmt"
+	"zwaLang/src/lexer"
+	"zwaLang/src/token"
+)
+
+// AST Node types
+type Node interface{}
+
+type VarDeclaration struct {
+	Name  string
+	Value int
+}
+
+type Assignment struct {
+	VarName string
+	Left    Node
+	Right   Node
+}
+
+type PrintStatement struct {
+	VarName string
+}
+
+// Parser structure
+type Parser struct {
+	lex 		*lexer.Lexer
+	curToken 	token.Token
+	peekToken 	token.Token
+}
+
+// NewParser initializes a parser with the given lexer
+func NewParser(lex *lexer.Lexer) *Parser {
+	p := &Parser{lex: lex}
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+// -- Receiver Functions --
+
+// NextToken advances the parser to the next token
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.lex.NextToken()
+}
+
+func (p *Parser) ParseProgram() []Node {
+	var nodes []Node
+	for p.curToken.Type != token.EOF {
+		node := p.parseStatement()
+		if node != nil {
+			nodes = append(nodes, node)
+		}
+		p.nextToken()
+	}
+	return nodes
+}
+
+func (p *Parser) parseStatement() Node {
+	switch p.curToken.Type {
+		case token.IDENT:
+			return p.parseAssignmentOrDeclaration()
+		case token.SHOW:
+			return p.parsePrintStatement()
+		default:
+			fmt.Printf("Unknown statement: %+v\n", p.curToken.Literal)
+			return nil
+	}
+}
+
+func (p *Parser) parseAssignmentOrDeclaration() Node {
+	identifier := p.curToken.Literal
+	p.nextToken()
+
+	if p.curToken.Type == token.COLON {
+		// Declaration
+		p.nextToken()
+		if p.curToken.Type != token.NATURAL_NUMBER_TYPE {
+			fmt.Printf("Expected type of variable (Currently only supports type: number) after colon: %+v\n", p.curToken.Literal)
+			return nil
+		}
+		p.nextToken()
+		if p.curToken.Type != token.ASSIGN {
+			fmt.Printf("Expected assignment operator after type declaration: %+v\n", p.curToken.Literal)
+			return nil
+		}
+		p.nextToken()
+		value := p.curToken.Literal
+		return &VarDeclaration{Name: identifier, Value: atoi(value)}
+	} else if p.curToken.Type == token.ASSIGN {
+		// Assignment -- currently only supports arithmetic assignment & very strict rules
+		p.nextToken()
+		if p.curToken.Type != token.IDENT {
+			fmt.Printf("Expected identifier after assignment operator: %+v\n", p.curToken.Literal)
+			return nil
+		}
+		left := p.curToken.Literal
+		
+		p.nextToken()
+		// TODO: Add support for more operators & to end here with re-assignment if a variable is being re-assigned
+		if p.curToken.Type != token.PLUS {
+			fmt.Printf("Expected arithmetic operator after assignment operator: %+v\n", p.curToken.Literal)
+			return nil
+		}
+		
+		p.nextToken()
+		if p.curToken.Type != token.IDENT {
+			fmt.Printf("Expected identifier after arithmetic operator: %+v\n", p.curToken.Literal)
+			return nil
+		}
+		right := p.curToken.Literal
+		return &Assignment{VarName: identifier, Left: left, Right: right}
+	}
+
+	return nil
+}
+
+func (p *Parser) parsePrintStatement() Node {
+	p.nextToken()
+	// TODO: Add support for printing more than just a variable
+	if p.curToken.Type != token.IDENT {
+		fmt.Printf("Expected identifier after print statement: %+v\n", p.curToken.Literal)
+		return nil
+	}
+	return &PrintStatement{VarName: p.curToken.Literal}
+}
+
+// -- Helper Functions --
+
+func atoi(str string) int {
+	var result int
+	fmt.Sscanf(str, "%d", &result)
+	return result
+}
--- src/zwaLang.go
+++ src/zwaLang.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"zwaLang/src/lexer"
-	"zwaLang/src/token"
+	//"zwaLang/src/token"
+	"zwaLang/src/parser"
 )
 
 // -- Main --
@@ -16,8 +17,15 @@ func main() {
 	fmt.Println(testInputString)
 
 	l := lexer.NewLexer(testInputString)
+	// for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+	// 	fmt.Printf("%+v\n", tok)
+	// }
 
-	for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-		fmt.Printf("%+v\n", tok)
+	p := parser.NewParser(l)
+	fmt.Printf("%+v\n", p)
+	ast := p.ParseProgram()
+
+	for _, node := range ast {
+		fmt.Printf("%+v\n", node)
 	}
 }
